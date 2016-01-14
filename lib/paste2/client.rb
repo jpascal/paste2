cb7d4fa8fc1dcc623a23e1af593cbb596431932b
Evgeniy Shurmin <eshurmin@gmail.com>
2016-01-14 15:11:39 +0300

    fix quoting strings, add language types for posting

--- lib/paste2/client.rb
+++ lib/paste2/client.rb
@@ -5,26 +5,26 @@ class Paste2::Client
   def self.post(code, description = '', lang = 'text')
     uri = URI(Paste2::URL)
     res = Net::HTTP.post_form(uri, :code => code, :description => description, :lang => lang)
-    File.join(Paste2::URL,res["location"])
+    File.join(Paste2::URL,res['location'])
   end
   def initialize
     @options = {}
     parser = OptionParser.new do|opts|
-      opts.separator ""
-      opts.separator "Examples:"
+      opts.separator ''
+      opts.separator 'Examples:'
       opts.separator "    echo 'code' | paste2"
-      opts.separator "    paste2 < file"
-      opts.separator ""
-      opts.separator "Specific options:"
-      opts.banner = "Usage: paste2 [options]"
+      opts.separator '    paste2 < file'
+      opts.separator ''
+      opts.separator 'Specific options:'
+      opts.banner = 'Usage: paste2 [options]'
       opts.on( '-f', '--file FILE', 'Post content from file' ) do|file|
         @options[:file] = file
       end
       opts.on( '-d', '--description TEXT', 'Description for post' ) do|description|
         @options[:description] = description
       end
-      opts.on( '-a', '--all', 'List supported languages' ) do|file|
-        puts "Supported languages:"
+      opts.on( '-a', '--all', 'List supported languages' ) do
+        puts 'Supported languages:'
         Paste2::LANGUAGES.each do |lang, name|
           printf "\t%-20s %s\n", lang, name
         end
@@ -55,7 +55,7 @@ class Paste2::Client
     return if @options[:help]
     begin
       code ||= @options[:file].nil? ? STDIN.readlines : File.open(@options[:file]).readlines
-      puts Paste2::Client.post(code.join(""), @options[:description], @options[:language])
+      puts Paste2::Client.post(code.join(''), @options[:description], @options[:language])
     rescue => e
       puts e.message
       exit(1)
--- paste2.gemspec
+++ paste2.gemspec
@@ -4,20 +4,20 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'paste2/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "paste2"
+  spec.name          = 'paste2'
   spec.version       = Paste2::VERSION
-  spec.authors       = ["Evgeniy Shurmin "]
-  spec.email         = ["eshurmin@gmail.com"]
+  spec.authors       = ['Evgeniy Shurmin ']
+  spec.email         = ['eshurmin@gmail.com']
   spec.description   = %q{Utility allow create posts on Paste2.org}
   spec.summary       = %q{Utility allow create posts on Paste2.org}
-  spec.homepage      = "http://github.com/jpascal/paste2"
-  spec.license       = "MIT"
+  spec.homepage      = 'http://github.com/jpascal/paste2'
+  spec.license       = 'MIT'
 
   spec.files         = `git ls-files`.split($/)
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
-  spec.add_development_dependency "bundler", "~> 1.3"
-  spec.add_development_dependency "rake"
+  spec.add_development_dependency 'bundler', '~> 1.3'
+  spec.add_development_dependency 'rake'
 end
